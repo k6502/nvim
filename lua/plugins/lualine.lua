79be4bd16b29e64cc3e74ddf14b2ace3f4757195
k6502 <dev@kthread.dev>
2026-02-21 13:16:47 -0300

    fix

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,6 @@ return {
 	"nvim-lualine/lualine.nvim",
 	lazy = false,
 	config = function()
-		local triforce = require("triforce.lualine").components()
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
