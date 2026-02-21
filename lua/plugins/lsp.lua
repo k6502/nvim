99b46901a3d87d25d5304e61c22e18cf1b231a97
k6502 <dev@kthread.dev>
2026-02-21 11:20:11 -0300

    markview

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -114,7 +114,7 @@ return {
 			-- Default list of enabled providers defined so that you can extend it
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
-				default = { "lsp", "path", "snippets", "buffer", "cmdline", "omni" },
+				default = { "lsp", "path", "snippets", "buffer", "cmdline", "omni", "markview" },
 			},
 
 			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
